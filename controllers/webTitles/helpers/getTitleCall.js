c0dcb793234e3c1616ec955fe6eb085edc11a294
babar.0khan1@gmail.com <babar.0khan1@gmail.com>
2021-04-01 16:01:40 +0500

    converted to callbacks

--- controllers/webTitles/helpers/getTitleCall.js
+++ controllers/webTitles/helpers/getTitleCall.js
@@ -1,9 +1,10 @@
 const { Value } = require("baconjs");
 const { callApi } = require("../../../Ghelpers/callApi");
-
+const request=require('request')
 
 exports.getTitleCall = async (req, res) => {
     try {
+        console.log("Babar");
         if (req.query.address) {
             let finalArray = [];
             let isArr = Array.isArray(req.query && req.query.address);
@@ -11,21 +12,28 @@ exports.getTitleCall = async (req, res) => {
             let result = null
             isArr ? urlArray = req.query.address : urlArray = [req.query.address]
 
-            for (let i = 0; i < urlArray.length; i++) {
-                try {
-                    result = await callApi("https://" + urlArray[i])
-                        ;
-                    if (result)
-                        finalArray.push({ link: "https://" + urlArray[i], valid: true })
-                } catch (e) {
-                    finalArray.push({ link: "https://" + urlArray[i], valid: false })
+          for (let i=0;i<urlArray.length;i++){
+              try{
+            request("https://"+urlArray[i], function (error, response, body) {
+               if(error){
+                   
+                finalArray.push({link:urlArray[i],valid:false})
+                if(finalArray.length==urlArray.length){
+                    return res.json({success:true,data:finalArray})
                 }
+               }
+               if(response&&response.statusCode==200){
+                   finalArray.push({link:urlArray[i],valid:true})
+                   if(finalArray.length==urlArray.length){
+                    return res.json({success:true,data:finalArray})
+                }
+              
+               }
+              });
+            }catch(e){
+                console.log(e);
             }
-            return res.json({
-                success: true,
-                data: finalArray
-            })
-
+          }
 
         } else {
             throw new Error("please enter query parameter");
@@ -36,3 +44,4 @@ exports.getTitleCall = async (req, res) => {
         res.json({ error: e.message })
     }
 }
+
